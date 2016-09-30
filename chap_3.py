b2f7ea33ada7c6fa82ac2bbc55d11042c1093bb3
dsharp <dsshar@gmail.com>
2016-09-30 18:30:54 -0400

    Added examples code from python machine learning

---
+++ chap_3.py
@@ -0,0 +1,40 @@
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.2):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 0].min()  -1, X[:, 0].max() + 1
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+sc = StandardScaler()
+sc.fit(X_train)
+
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+
+ppn.fit(X_train_std, y_train)
+
+y_pred = ppn.predict(X_test_std)
+#print((y_test != y_pred).sum())
+print('Misclassified samples: {0}'.format((y_test != y_pred).sum()))
+
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
